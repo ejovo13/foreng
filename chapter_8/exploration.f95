cd254bc43c14d82bd8d0d400c804f2124684c604
Evan Voyles <ejovo13@yahoo.com>
2021-03-02 16:01:24 +0100

    Played around with multidimensional arrays

--- chapter_8/exploration.f95
+++ chapter_8/exploration.f95
@@ -9,6 +9,10 @@ program exploration
     integer :: i
     integer, dimension(10) :: my_array = [ (i, i = 1,10)]
     logical, dimension(10) :: my_logic
+    integer, dimension(2,2) :: A
+    
+    A(1,:) = [ 1, 2 ]
+    A(2,:) = [ 3, 4 ]
 
     my_logic = my_array > 5
 
@@ -25,6 +29,14 @@ program exploration
 
     print *, "My new array = ", my_array
 
+    print *, "---------------------------"
+    print *, "Now testing rank 2 arrays"
+
+    print *, "Default representation of A :: "
+    print *, "A = ", A
+
+    print 100, (A(i,:), i = 1,2)
+    100 format(2(I0, 1X))
 
 end program
 
