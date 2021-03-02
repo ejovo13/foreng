8fa19a606c03150c49eff5543c26f45c16bc9bfa
Evan Voyles <ejovo13@yahoo.com>
2021-03-02 19:34:33 +0100

    Finish matrix multiplication code

--- chapter_8/8_13.f95
+++ chapter_8/8_13.f95
@@ -24,12 +24,14 @@ program matrix_multiplication
 implicit none
 
 ! Data dictionary
-real, dimension(:,:), allocatable :: A, B
+real, dimension(:,:), allocatable :: A, B, C        ! A and B are the matrices to be multiplied, C is the product
 integer :: rowA, colA, rowB, colB
 integer :: aerr, berr, i
+
 character(100) :: amsg, bmsg
 
-
+print 111
+print 999
 ! Try and open mat1.dat and mat2.dat
 open(unit=10, file='mat1.dat', status='old', action='read', iostat=aerr, iomsg=amsg)
 open(unit=11, file='mat2.dat', status='old', action='read', iostat=berr, iomsg=bmsg)
@@ -41,25 +43,51 @@ if (aerr == 0 .and. berr == 0) then ! Open was succesful
     
     print *, "The row and col of A is: ", rowA, colA
     print *, "The row and col of B is: ", rowB, colB
+    print 999
 
     if (colA == rowB) then
         print *, "Arrays are compatible, continuing with computation"
+        print 999
+        print 111
     else 
         stop "Arrays are not compatible for multiplication"
+        print 999
     end if
 
     allocate(A(rowA, colA))
     allocate(B(rowB, colB))
 
-    read(10, *) (A(i, :), i = 1,colA)
-    read(11, *) (B(i, :), i = 1,colB)
-
-    print *, A, B
+    read(10, *) (A(i, :), i = 1,rowA)
+    read(11, *) (B(i, :), i = 1,rowB)
 
 
+    print *, "Matrix A read in as: "
+    print 999
 
+    do i = 1,rowA
+        print *, A(i,:)
+    end do
 
+    print 111
+    print *, "Matrix B read in as: "
+    print 999
 
+    do i = 1, rowB
+        print *, B(i,:)
+    end do 
+
+    C = matmul(A, B) ! C gets automatically allocated by Fortran
+
+    print 111
+    print *, "A x B = "
+    print 999
+    ! C should be of size rowA x colB
+    do i = 1, rowA
+        print *, C(i,:)
+    end do
+
+    999 FORMAT(50("-"))
+    111 FORMAT(/)
 
     
 else if (aerr /= 0) then
