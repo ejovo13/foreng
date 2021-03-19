cfc040702cf9ece99526b437a1677067c3a085ff
Evan Voyles <ejovo13@yahoo.com>
2021-03-19 04:23:09 +0100

    Preliminary questions

---
+++ chapter_11/11_4.f95
@@ -0,0 +1,6 @@
+! What is an ill-conditioned system of equations? Why is it hard to find the solution to an
+! ill-conditioned set of equations.
+
+! An ill-conditioned system of equations is one where some of the coefficients are 
+! unknown. Since we are missing crucial elements in the matrix that we would like to 
+! solve, finding the solution becomes much more complicated.
