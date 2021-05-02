882856c07655f4f6f465ecb2b40b46991c8f20c4
Evan Voyles <ejovo13@yahoo.com>
2021-05-02 18:16:18 +0200

    Bye

--- src/ch4/4_6.f90
+++
@@ -1,4 +0,0 @@
-! What is the difference in behaviour between a cycle statement and an exit statement?
-
-! A cycle statement sends the program control to the start of the loop, whereas exit exits the 
-! loop entirely.
