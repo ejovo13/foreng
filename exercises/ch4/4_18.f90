d014b3f6af4c31eb003a19339c5e94ecc8f972fa
Evan Voyles <ejovo13@yahoo.com>
2021-05-03 23:40:16 +0200

    Refactor

--- exercises/ch4/4_18.f90
+++ exercises/ch4/4_18.f90
@@ -44,16 +44,16 @@ implicit none
 
 contains 
 
-    elemental function calc_current_flow(v_D, I_O, T) result(i_D)
+    elemental function calc_current_flow(v_D_, I_O_, T_) result(i_D_)
     ! Data Dictionary
-        real(16) :: i_D                              ! The current flow through the diode in amperes
-        real(16), intent(in) :: v_D                  ! The voltage across the diode, in volts
-        real(16), intent(in) :: I_O                  ! The leakage current of the diode, in amperes
-        real(16), intent(in) :: T                    ! Temperature, in kelvins (K)
-        real(16), parameter :: q = 1.602E-19         ! The charge on an electron, 1.602 X 10-19 Coulombs
-        real(16), parameter :: k = 1.38E-23          ! Boltzmann's constant, 1.38 X 10-23 J/K
+        real(16) :: i_D_                              ! The current flow through the diode in amperes
+        real(16), intent(in) :: v_D_                  ! The voltage across the diode, in volts
+        real(16), intent(in) :: I_O_                 ! The leakage current of the diode, in amperes
+        real(16), intent(in) :: T_                    ! Temperature, in kelvins (K)
+        real(16), parameter :: q_ = 1.602E-19         ! The charge on an electron, 1.602 X 10-19 Coulombs
+        real(16), parameter :: k_ = 1.38E-23          ! Boltzmann's constant, 1.38 X 10-23 J/K
         
-        i_D = I_O * (exp((q * v_D)/(k*T)) - 1)
+        i_D_ = I_O_ * (exp((q_ * v_D_)/(k_*T_)) - 1)
 
     end function
 
