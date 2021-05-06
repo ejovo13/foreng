4c86dfa645ecf7a9c2c6c728b7ab28516f4881cf
Evan Voyles <ejovo13@yahoo.com>
2021-05-06 20:00:33 +0200

    Update docs

--- docs/src/math_prob.f90
+++ docs/src/math_prob.f90
@@ -9,7 +9,7 @@ implicit none
 !=                      Random Distribution Interface                        =!
 !=============================================================================!
 interface urand
-!! Return a uniform random variable from min to max
+!! Return a uniform random variable from min (inclusive) to max (inclusive for integer functions, exclusive for real)
     module procedure urand_r32
     module procedure urand_r64
     module procedure urand_i32
@@ -18,7 +18,19 @@ interface urand
 end interface 
 
 interface fill_urand
-    
+!! Fill a rank 1, 2, or 3 array with a uniform random variable from min (inclusive) to max (inclusive for integer functions, exclusive for real)
+!!
+!! Example usage:
+!!```fortran
+!! 
+!!integer, dimension(64) :: x_i32
+!!real(real32), dimension(8, 8) :: x_r32
+!!real(real64), dimension(4, 4, 4) :: x_64
+!!
+!!call fill_urand(x_i32, 0, 10) ! Fill rank1, integer array
+!!call fill_urand(x_r32, -1.0, 1.0) ! Fill rank2, real(real32) array
+!!call fill_urand(x_r64, -10.d0, 10.d0) ! Fill rank3, real(real64) array
+!!```
     module procedure fill_urand_rank1_r32
     module procedure fill_urand_rank1_r64
     module procedure fill_urand_rank1_i32
@@ -44,9 +56,11 @@ end interface
 !=============================================================================!
 contains
 
-    real(real32) function urand_r32(min,max)
-
-        real(real32), intent(in) :: min, max
+    function urand_r32(min,max) result(x)
+    !! Return a uniform random variable [min, max)
+        real(real32), intent(in) :: min !! \(a\), inclusive
+        real(real32), intent(in) :: max !! \(b\), exclusive
+        real(real32) :: x !! \(x \sim \mathcal{U}[a, b)\)
 
         real(real32) :: temp_rand ! A value between 0 and 1
         real(real32), parameter :: rand_mid = 0.5
@@ -61,13 +75,15 @@ contains
         ! print *, temp_rand
 
         ! Shift the midpoint to the midpoint of the new range
-        urand_r32 = temp_rand*scale + shift   
+        x = temp_rand*scale + shift   
 
     end function
 
-    real(real64) function urand_r64(min,max)
-
-        real(real64), intent(in) :: min, max
+    function urand_r64(min,max) result(x)
+    !! Return a uniform random variable [min, max)
+        real(real64), intent(in) :: min !! \(a\), inclusive
+        real(real64), intent(in) :: max !! \(b\), exclusive
+        real(real64) :: x !! \(x \sim \mathcal{U}[a, b)\)
 
         real(real64) :: temp_rand ! A value between 0 and 1
         real(real64), parameter :: rand_mid = 0.5
@@ -82,13 +98,15 @@ contains
         ! print *, temp_rand
 
         ! Shift the midpoint to the midpoint of the new range
-        urand_r64 = temp_rand*scale + shift   
+        x = temp_rand*scale + shift   
 
     end function
 
-    integer(int32) function urand_i32(min, max)
+    function urand_i32(min, max) result(x)
     !! Return a uniform random variable [min, max] 
-        integer(int32), intent(in) :: min, max
+        integer(int32), intent(in) :: min !! \(a\), inclusive
+        integer(int32), intent(in) :: max !! \(b\), inclusive
+        integer(int32) :: x !! \(x \sim \mathcal{U}[a, b]\)
 
         real(real32) :: temp_rand ! A value between 0 and 1
         real(real32), parameter :: rand_mid = 0.5
@@ -103,13 +121,15 @@ contains
         ! print *, temp_rand
 
         ! Shift the midpoint to the midpoint of the new range
-        urand_i32 = int(temp_rand*scale + shift, int32)   
+        x = int(temp_rand*scale + shift, int32)   
 
     end function
 
-    integer(int64) function urand_i64(min, max)
-
-        integer(int64), intent(in) :: min, max
+    function urand_i64(min, max) result(x)
+    !! Return a uniform random variable [min, max] 
+        integer(int64), intent(in) :: min !! \(a\), inclusive
+        integer(int64), intent(in) :: max !! \(b\), inclusive
+        integer(int64) :: x !! \(x \sim \mathcal{U}[a, b]\)
 
         real(real64) :: temp_rand ! A value between 0 and 1
         real(real64), parameter :: rand_mid = 0.5
@@ -124,7 +144,7 @@ contains
         ! print *, temp_rand
 
         ! Shift the midpoint to the midpoint of the new range
-        urand_i64 = int(temp_rand*scale + shift, int64)   
+        x = int(temp_rand*scale + shift, int64)   
 
     end function
 
--- src/foreng/math_misc.f90
+++ src/foreng/math_misc.f90
@@ -17,7 +17,7 @@ end interface
 !=                            nth Root Interface                             =!
 !=============================================================================!
 interface nth_root
-!! Compute \(x!\)
+!! Compute \(\sqrt[n]{x}\)
     module procedure nth_root_r32
     module procedure nth_root_r64
 end interface
@@ -26,7 +26,7 @@ end interface
 !=                     Infinite Series Interface                             =!
 !=============================================================================!
 interface exp_series
-!! Compute \(x!\)
+!! Compute \(e^x\) using a truncated taylor series
     module procedure exp_series_r32
     module procedure exp_series_r64
 end interface
@@ -39,13 +39,17 @@ end interface
 contains
 
     recursive function factorial_int16(x) result (x_fact)
-        
-        integer(int16), intent(in) :: x
+    !! Recursively compute the factorial of a 16-bit integer. The max value that can be passed is 7
+    !! If x is less than 0 or greater 7, the function will return 0
+        integer(int16), intent(in) :: x !! \( 0 \leq x \leq 7\)
         integer(int16) :: x_fact !! \(x!\)
 
         if ( x >= 8) then 
             x_fact = 0
             return 
+        else if ( x < 0) then
+            x_fact = 0
+            return
         end if
 
 
@@ -58,13 +62,17 @@ contains
     end function
 
     recursive function factorial_int32(x) result (x_fact)
-
-        integer(int32), intent(in) :: x
+    !! Recursively compute the factorial of a 32-bit integer. The max value that can be passed is 16
+    !! If x is less than 0 or greater 16, the function will return 0
+        integer(int32), intent(in) :: x !! \( 0 \leq x \leq 16 \)
         integer(int32) :: x_fact !! \(x!\)
 
         if ( x >= 17) then 
             x_fact = 0
             return 
+        else if ( x < 0) then
+            x_fact = 0
+            return
         end if
 
         if (x == 0) then
@@ -76,13 +84,17 @@ contains
     end function
 
     recursive function factorial_int64(x) result (x_fact)
-
-        integer(int64), intent(in) :: x
+    !! Recursively compute the factorial of a 64-bit integer. The max value that can be passed is 20
+    !! If x is less than 0 or greater 20, the function will return 0
+        integer(int64), intent(in) :: x !! \( 0 \leq x \leq 20 \)
         integer(int64) :: x_fact !! \(x!\)
 
         if ( x >= 21) then 
             x_fact = 0
             return 
+        else if ( x < 0) then
+            x_fact = 0
+            return
         end if
 
         if (x == 0) then
@@ -94,29 +106,33 @@ contains
     end function
 
     recursive function factorial_int128(x) result (x_fact)
-    !! Supported for gnufortran compiler with kind = 16
-    integer(int128), intent(in) :: x
-    integer(int128) :: x_fact !! \(x!\)
+    !! Recursively compute the factorial of a 128-bit integer [supported on gfortran]. The max value that can be passed is 33
+    !! If x is less than 0 or greater 33, the function will return 0
+        integer(int128), intent(in) :: x !! \( 0 \leq x \leq 33 \)
+        integer(int128) :: x_fact !! \(x!\)
 
-    if ( x >= 34) then 
-        x_fact = 0
-        return 
-    end if
+        if ( x >= 34) then 
+            x_fact = 0
+            return 
+        else if ( x < 0) then
+            x_fact = 0
+            return
+        end if
 
-    if (x == 0) then
-        x_fact = 1_int128
-        return
-    end if
-    x_fact = x * factorial(x - 1_int128)
+        if (x == 0) then
+            x_fact = 1_int128
+            return
+        end if
+        x_fact = x * factorial(x - 1_int128)
 
-end function
+    end function
 
 !=============================================================================!
 !=                     Infinite Series Functions                             =!
 !=============================================================================!
 
     real function exp_series_r32(x) result(exp)
-    !! Calculate e^x using a taylor series with 12 terms
+    !! Compute \(e^x\) using a taylor series with 12 terms
 
         real(real32), intent(in) :: x
 
@@ -134,7 +150,7 @@ end function
     end function
 
     real(real64) function exp_series_r64(x) result(exp)
-    !! Calculate e^x using a taylor series with 20 terms
+    !! Calculate \(e^x\) using a taylor series with 20 terms
 
         real(real64), intent(in) :: x
 
